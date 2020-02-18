e1ba7bfe301796dbdbd36bed9fa69c5487027231
Edgar Rupare <erupar200@caledonian.ac.uk>
2020-02-18 02:08:14 +0000

    Updating main.tf

---
+++ README.md
@@ -0,0 +1,11 @@
+
+### Add .gitignore to gitignore
+```gitignore
+ git rm --cached .gitignore 
+```
+
+
+### Add credential helper
+```bash
+git config --global credential.helper store 
+```
--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ provider "aws" {
 //}
 
 resource "aws_instance" "web" {
-  ami           = ami-0c322300a1dd5dc79
+  ami           = "ami-0c322300a1dd5dc79"
   instance_type = "t2.micro"
   tags = {
     Name = "Wolverine"
