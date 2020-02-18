b2086fcccda24e6fc57fb4e07500a584a3382a4c
Edgar Rupare <erupar200@caledonian.ac.uk>
2020-02-18 01:40:16 +0000

    Updating main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,37 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-rachel"
+    organization = "edgar-training"
     workspaces {
-      name = "sentinel-demo1"
+      name = "Sentinel"
     }
   }
 }
 
 provider "aws" {
-  region                  = "us-west-2"
+  region                  = "us-east-1"
   profile                 = "default"
   shared_credentials_file = "~/.aws/credentials"
 }
 
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
+//data "aws_ami" "ubuntu" {
+//  most_recent = true
+//
+//  filter {
+//    name   = "name"
+//    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+//  }
+//
+//  filter {
+//    name   = "virtualization-type"
+//    values = ["hvm"]
+//  }
+//
+//  owners = ["099720109477"] # Canonical
+//}
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = ami-0c322300a1dd5dc79
   instance_type = "t2.micro"
   tags = {
     Name = "Wolverine"
